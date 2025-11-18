b89fd658423a7161560b8f0ce624f396227be178
nnloi <nnloi@liink.vn>
2025-11-18 11:21:37 +0700

    update API key

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from google import genai
 from google.genai import types
 
 # Lấy API Key từ biến môi trường
-GEMINI_API_KEY = "AIzaSyAUMkuhN4KxQQoOCWd6bX0rsuIBP0fGNBE"
+GEMINI_API_KEY = "AIzaSyCo4aUx_zwv9LSobwnlJ4XI6OfyVOcZOOU"
 
 # Khởi tạo client Gemini
 client = genai.Client(api_key=GEMINI_API_KEY)
